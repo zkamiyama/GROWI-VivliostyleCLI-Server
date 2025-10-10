555a38da3ef491b1dcfd92ea7e2cae5dd25d1798
zkamiyama <kmzk.metatron@gmail.com>
2025-10-10 14:16:21 +0900

    fix: increase max memory restart limit for vivliostyle-cli-server

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '512M',
+      max_memory_restart: '2048M',
       env: {
         NODE_ENV: 'production',
         PORT: process.env.PORT || 4781,
